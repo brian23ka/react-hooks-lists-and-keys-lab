89ff8954c8c0f0263d9341d934cf632046f29229
Brian Kaloki <brianngenye@gmail.com>
2025-04-09 11:09:38 +0300

    myu first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,47 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+import user from "../data/user";  // Assuming this file contains user data
+
+const projects = [
+  {
+    id: 1,
+    name: "Project One",
+    description: "A cool web app.",
+    technologies: ["React", "Node", "CSS"]
+  },
+  {
+    id: 2,
+    name: "Project Two",
+    description: "Another awesome project.",
+    technologies: ["Vue", "JavaScript", "SASS"]
+  },
+  {
+    id: 3,
+    name: "Project Three",
+    description: "Yet another amazing project.",
+    technologies: ["Angular", "TypeScript", "HTML"]
+  }
+];
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home 
+        name={user.name} 
+        city={user.city} 
+        color={user.color} 
+      />
+      <About 
+        bio={user.bio} 
+        github={user.links.github} 
+        linkedin={user.links.linkedin} 
+      />
+      <ProjectList projects={projects} />
     </div>
   );
 }
 
 export default App;
+
